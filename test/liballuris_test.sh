e559c6e927092f1c82ac767b857d69fc01fe85d2
Andreas Weber <weber@alluris.de>
2015-07-27 12:33:48 +0200

    Add more tests

--- test/liballuris_test.sh
+++ test/liballuris_test.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
 
 ## set defaults
-gadc --stop
-gadc --set-pos-limit 0
-gadc --set-neg-limit 0
+../cli/gadc --stop
+../cli/gadc --set-pos-limit 0
+../cli/gadc --set-neg-limit 0
 
 ## check pos limit
-gadc --set-pos-limit 321
-export value=$(gadc --get-pos-limit)
+../cli/gadc --set-pos-limit 321
+export value=$(../cli/gadc --get-pos-limit)
 if (($value != 321)); then
   printf "set-pos-limit FAILED\n"
   exit 1;
 fi
 
 ## check neg limit
-gadc --set-neg-limit 4422
-export value=$(gadc --get-neg-limit)
-if (($value != 4422)); then
+../cli/gadc --set-neg-limit -123
+export value=$(../cli/gadc --get-neg-limit)
+if (($value != -123)); then
   printf "set-neg-limit FAILED\n"
   exit 1;
 fi
 
-## Start measurement, read one value
-gadc --start
-gadc --value
+## Start measurement, read one value, stop
+../cli/gadc --start
+../cli/gadc --value
+../cli/gadc --stop
 
-gadc --pos-peak
-gadc --neg-peak
+## in one command
+../cli/gadc --start -v --stop
 
-gadc --tare
-gadc --clear-neg
-gadc --clear-pos
+## get pos and neg peaks
+../cli/gadc --start
 
-gadc --stop
+../cli/gadc --pos-peak --neg-peak
+
+../cli/gadc --tare
+../cli/gadc --clear-neg
+../cli/gadc --clear-pos
+
+../cli/gadc --stop
