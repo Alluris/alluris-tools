4d0565fc17ad700ac3d71f76da865094b5d6a455
Andreas Weber <weber@alluris.de>
2016-06-21 09:25:26 +0200

    gadc.m: Add error

--- examples/gadc.m
+++ examples/gadc.m
@@ -1,17 +1,17 @@
 ## Copyright (C) 2015 Alluris GmbH & Co. KG <weber@alluris.de>
-## 
+##
 ## This file is part of liballuris.
-## 
+##
 ## Liballuris is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU Lesser General Public License as published by
 ## the Free Software Foundation, either version 3 of the License, or
 ## (at your option) any later version.
-## 
+##
 ## Liballuris is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ## GNU Lesser General Public License for more details.
-## 
+##
 ## You should have received a copy of the GNU Lesser General Public License
 ## along with liballuris. See ../COPYING.LESSER
 ## If not, see <http://www.gnu.org/licenses/>.
@@ -29,11 +29,24 @@ function ret = gadc (cmd)
   endif
 
   [s, ret] = system ( sprintf ("gadc %s", cmd));
-  
+
   if (s != 0)
-    error ("gadc call failed: returned %i", s)
+    ## liballuris.h, liballuris_error
+    msg = {"LIBALLURIS_MALFORMED_REPLY",
+           "LIBALLURIS_DEVICE_BUSY",
+           "LIBALLURIS_TIMEOUT",
+           "LIBALLURIS_OUT_OF_RANGE"};
+    if (s < 0)
+      msg_tmp = "LIBUSB ERROR";
+    elseif (s > 0 && s <= numel (msg))
+      msg_tmp = msg{s};
+    else
+      msg_tmp = "UNKNOWN ERROR";
+    endif
+
+    error ("gadc call failed: returned %i = %s", s, msg_tmp)
   endif
-  
+
   if (! isempty (ret))
     ## remove last \n
     ret(end)=[];
